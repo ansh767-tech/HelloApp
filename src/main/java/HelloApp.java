5679364ec0e1d014d94846f4e32c0a071d9eb738
ansh767-tech <therealansh767@gmail.com>
2026-03-26 13:56:58 +0530

    Display Hello with command-line argument or default message

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,10 +1,9 @@
-public class HelloApp{
-public static void main(String[] args) {
-    if (args.length > 0) {
-        String name = args[0];
+public class HelloApp {
+    public static void main(String[] args) {
+        // Use a ternary operator to assign the name or default to "World"
+        String name = (args.length > 0) ? args[0] : "World";
+        
+        // Print the final personalized or default greeting
         System.out.println("Hello, " + name + "!");
-    } else {
-        System.out.println("Please provide a name as a command-line argument.");
     }
-}
 }
